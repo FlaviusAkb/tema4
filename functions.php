ca8f8265eef170810d27ca9055e6972d28e032e0
FlaviusAkb <46058216+FlaviusAkb@users.noreply.github.com>
2022-08-18 08:27:54 +0300

    redone

--- functions.php
+++ functions.php
@@ -1,68 +1,13 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles () {
-    wp_enqueue_style( 'astra-child', get_stylesheet_uri(),
-        array( 'astra' ), 
-        wp_get_theme()->get('Version') // this only works if you have Version in the style header
-    );   
+function akb_add_style() {
+    wp_enqueue_style('akb-style', get_stylesheet_directory_uri() . '/style.css');
 }
+add_action('wp_enqueue_scripts', 'akb_add_style');
+
 
 add_action( 'astra_html_before','add_contact_navbar' );
 function add_contact_navbar () { ?> 
-
-<style>
-
-ul {
-    margin: 0 1em!important;
-}
-
-#wpr-top-header {
-    background: #0E3C52!important;
-    height: 40px;
-    position: sticky!important;
-    top: 0!important;
-}
-.wpr-container-top-navbar {
-    height: inherit;
-    display: flex;
-    justify-content: space-between;
-}
-.wpr-container-top-navbar ul {
-    height: inherit;
-    list-style-type: none;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-}
-.wpr-menu-item
- {
-    margin-right: 1rem!important;
-    
-}
-.wpr-menu-item a
- {
-
-}
-
-.wpr-menu-item a:hover,
-.wpr-navbar-left-item a:hover {
-    color: #0E3C52!important;
-    background-color: #fff!important;
-}
-
-.wpr-navbar-left-item a, 
-.wpr-menu-item a {
-    color: #fff!important;
-    font-weight: 600;
-}
-
-h2 {
-    font-weight: 600;
-}
-    </style>
-    
 <div id="wpr-top-header">
 		<div class="wpr-container-top-navbar">
 			<ul class="wpr-navbar-left">
